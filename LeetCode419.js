c7378ddc68b02746d7731aa908262909b8a8f871
lxfxf <xliu222@aucklanduni.ac.nz>
2017-04-21 22:18:00 +1200

    419 is AC! Go to bed.

--- LeetCode419.js
+++ LeetCode419.js
@@ -5,6 +5,40 @@
  * @param {character[][]} board
  * @return {number}
  */
-var countBattleships = function(board) {
 
+
+/**
+ *  AC, beats 31.17%
+ *  If a 'X' up and left ([i][j - 1] and [i - 1][j]) slot is '.', this 'X' is a new head of a battleship.
+ *  Ohterwise, this 'X' is a part of a battleship.
+ */
+var countBattleships = function(board) {
+    var battleshipsCount = 0;
+    var row = board.length;
+    var columns = board[0].length;
+    for (var i = 0; i < row; i++) {
+        for (var j = 0; j < columns; j++) {
+            if (board[i][j] === 'X'){
+                if (i > 0 && j > 0) {
+                    if (board[i][j - 1] !== 'X' && board[i - 1][j] !== 'X') {
+                        battleshipsCount++;
+                    }
+                }
+                else if (i === 0 && j > 0) {
+                    if (board[i][j-1] !== 'X') {
+                        battleshipsCount++;
+                    }
+                }
+                else if (i > 0 && j === 0) {
+                    if (board[i - 1][j] !== 'X') {
+                        battleshipsCount++;
+                    }
+                }
+                else if (i === 0 && j === 0) {
+                    battleshipsCount++;
+                }
+            }
+        }
+    }
+    return battleshipsCount;
 };
