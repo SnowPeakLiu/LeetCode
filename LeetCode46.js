5fc0527125032408e800f8d5007b619f24e7adbe
SnowPeakLiu <xliu222@aucklanduni.ac.nz>
2017-05-04 21:59:39 +1200

    No.46 is AC!

--- LeetCode46.js
+++ LeetCode46.js
@@ -5,45 +5,49 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+// Johnson-Trotter Algorithm
+    // Beat 8.33%
 var permute = function(nums) {
     let permutation = [];
     let n = nums.length;
     let pi = [], d = [];
     let max = Math.max(...nums);
-    let min = Math.min(...nums);
 
+    nums.sort();
     for (let i = 1; i < n + 1; i ++) {
-        pi[i] = i;
+        pi[i] = nums[i - 1];
         d[i] = -1;
     }
-    pi[0] = n + 1;
-    pi[n + 1] = n + 1;
+    pi[0] = max + 1;
+    pi[n + 1] = max + 1;
     //console.log(pi);
     let set_A = new Set();
-    for (let i = 2; i <= n ; i++) {
-        set_A.add(i);
+    for (let i = 1; i < n ; i++) {
+        set_A.add(nums[i]);
     }
     //console.log(set_A);
     let done = false;
+    let bibi =1;
     while (!done) {
-        let adjustedPermutation = pi.map(i => (i + min - 1));
-        permutation.push(adjustedPermutation.slice(1, n + 1));
+
+        permutation.push(pi.slice(1, n + 1));
         if (set_A.size > 0) {
             let m = Math.max(...set_A);
             let j = pi.indexOf(m);
-            pi[j] = pi[j + d[m]];
-            pi[j + d[m]] = m;
+            let d_index = nums.indexOf(m) + 1;
+            pi[j] = pi[j + d[d_index]];
+            pi[j + d[d_index]] = m;
             //console.log('pi');
             //console.log(pi);
-            if (m < pi[j + 2 * d[m]]) {
-                d[m] = -d[m];
+            if (m < pi[j + 2 * d[d_index]]) {
+                d[d_index] = -d[d_index];
                 set_A.delete(m);
                 //console.log('delete m');
             }
             //console.log(set_A);
-            for (let i = m + 1; i <= n; i++) {
+            for (let i = d_index; i < n; i++) {
                 //console.log(max);
-                set_A.add(i);
+                set_A.add(nums[i]);        // not set_A.add(nums[d_index])!!!!!
             }
         }
         else {
@@ -52,3 +56,4 @@ var permute = function(nums) {
     }
     return permutation;
 };
+permute([1,2,3,4]);
